78ff9faf043f0dd214f2889542e90c97f432f597
Raymond Jackson <rjackson1989@temple.edu>
2016-03-20 17:10:07 -0400

    initial app state classes

---
+++ src/mygame/MainGame.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.renderer.RenderManager;
+
+/**
+ *
+ * @author Raymond
+ */
+public class MainGame extends AbstractAppState implements ActionListener, AnalogListener{
+
+    public void onAction(String name, boolean isPressed, float tpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void onAnalog(String name, float value, float tpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
---
+++ src/mygame/PauseScreen.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppState;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+
+/**
+ *
+ * @author Raymond
+ */
+public class PauseScreen extends AbstractAppState implements ActionListener, AnalogListener
+{
+
+    public void onAction(String name, boolean isPressed, float tpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void onAnalog(String name, float value, float tpf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
