8c1100cb5b518f556aca8bf04ce18e308ad3e190
Raymond Jackson <rjackson1989@temple.edu>
2016-03-27 14:40:25 -0400

    added basic movement

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -2,14 +2,17 @@ package mygame;
 
 import com.jme3.ai.agents.Agent;
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.ChaseCamera;
 import com.jme3.input.JoyInput;
 import com.jme3.input.Joystick;
 import com.jme3.input.JoystickAxis;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.JoyAxisTrigger;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
@@ -28,6 +31,8 @@ public class Main extends SimpleApplication implements AnalogListener{
     final int PAUSE = 2;
     final int GAME_OVER = 3;
     Geometry geom;
+    ChaseCamera ccam;
+    PlayerVehicle vehicle;
     
     public static void main(String[] args) {
         Main app = new Main();
@@ -40,16 +45,35 @@ public class Main extends SimpleApplication implements AnalogListener{
 
     @Override
     public void simpleInitApp() {
+        vehicle = new PlayerVehicle(this, 0);
         Box b = new Box(1, 1, 1);
         geom = new Geometry("Box", b);
         
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Blue);
         geom.setMaterial(mat);
+        geom.setLocalTranslation(0, 3, 0);
         initControls();
-        
-
+        initLighting();
+        flyCam.setEnabled(false);
+        Box c = new Box(100, 0, 100);
+        Geometry cube = new Geometry("Box2", c);
+        Material mat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat2.setColor("Color", ColorRGBA.Brown);
+        cube.setMaterial(mat2);
+        cube.setLocalTranslation(0, -1, 0);
+        ccam = new ChaseCamera(cam, vehicle.vehicleNode, inputManager);
         rootNode.attachChild(geom);
+        
+        rootNode.attachChild(cube);
+    }
+    
+    private void initLighting() {
+    viewPort.setBackgroundColor(ColorRGBA.LightGray);  
+    /** Add a light source so we can see the model */
+    DirectionalLight dl = new DirectionalLight();
+    dl.setDirection(new Vector3f(-0.1f, -1f, -1).normalizeLocal());
+    rootNode.addLight(dl);  
     }
     
     private void initControls()
@@ -61,9 +85,7 @@ public class Main extends SimpleApplication implements AnalogListener{
             throw new IllegalStateException("Cannot find Joystick!");
         }
         int i = 0;
-        
-//        joysticks[i].getAxis(JoystickAxis.X_AXIS).assignAxis("Right", "Left");
-//        joysticks[i].getAxis(JoystickAxis.Y_AXIS).assignAxis("Down", "Up");
+
        // for(int i = 0; i < 2; i++)
         //{
 inputManager.addMapping("LS Up", new JoyAxisTrigger(i, 0, true));
@@ -75,28 +97,28 @@ inputManager.addMapping("RS Up", new JoyAxisTrigger(i, 2, true));
 inputManager.addMapping("RS Down", new JoyAxisTrigger(i, 2, false));
 inputManager.addMapping("RS Left", new JoyAxisTrigger(i, 3, false));
 inputManager.addMapping("RS Right", new JoyAxisTrigger(i, 3, true));
-inputManager.addListener(this, "RS Left", "RS Right", "RS Down", "RS Up");
-inputManager.addMapping("DPAD Left", new JoyAxisTrigger(i, JoyInput.AXIS_POV_X, true));
-inputManager.addMapping("DPAD Right", new JoyAxisTrigger(i, JoyInput.AXIS_POV_X, false));
-inputManager.addMapping("DPAD Down", new JoyAxisTrigger(i, JoyInput.AXIS_POV_Y, true));
-inputManager.addMapping("DPAD Up", new JoyAxisTrigger(i, JoyInput.AXIS_POV_Y, false));
+inputManager.addListener(this, "RS Left", " cx  Q2WSRS Right", "RS Down", "RS Up");
+inputManager.addMapping("DPAD Left", new JoyAxisTrigger(i, 4, true));
+inputManager.addMapping("DPAD Right", new JoyAxisTrigger(i, 4, false));
+inputManager.addMapping("DPAD Down", new JoyAxisTrigger(i, 5, true));
+inputManager.addMapping("DPAD Up", new JoyAxisTrigger(i, 5, false));
 inputManager.addListener(this, "DPAD Left", "DPAD Right", "DPAD Down", "DPAD Up");
 inputManager.addMapping("Trigger L", new JoyAxisTrigger(i, 4, false));
 inputManager.addMapping("Trigger R", new JoyAxisTrigger(i, 4, true));
 inputManager.addListener(this, "Trigger L", "Trigger R");
-joysticks[i].assignButton("Button A", 0);
-joysticks[i].assignButton("Button B", 1);
-joysticks[i].assignButton("Button X", 2);
-joysticks[i].assignButton("Button Y", 3);
-inputManager.addListener(this, "Button A", "Button B", "Button X", "Button Y");
-joysticks[i].assignButton("Button LB", 4);
-joysticks[i].assignButton("Button RB", 5);
-joysticks[i].assignButton("Button Back", 6);
-joysticks[i].assignButton("Button Start", 7);
-inputManager.addListener(this, "Button LB", "Button RB", "Button Back", "Button Start");
-joysticks[i].assignButton("Button LS", 8 );
-joysticks[i].assignButton("Button RS", 9);
-inputManager.addListener(this, "Button LS", "Button RS");
+//joysticks[i].assignButton("Button A", 0);
+//joysticks[i].assignButton("Button B", 1);
+//joysticks[i].assignButton("Button X", 2);
+//joysticks[i].assignButton("Button Y", 3);
+//inputManager.addListener(this, "Button A", "Button B", "Button X", "Button Y");
+//joysticks[i].assignButton("Button LB", 4);
+//joysticks[i].assignButton("Button RB", 5);
+//joysticks[i].assignButton("Button Back", 6);
+//joysticks[i].assignButton("Button Start", 7);
+//inputManager.addListener(this, "Button LB", "Button RB", "Button Back", "Button Start");
+//joysticks[i].assignButton("Button LS", 8 );
+//joysticks[i].assignButton("Button RS", 9);
+//inputManager.addListener(this, "Button LS", "Button RS");
        // }
 
     }
@@ -112,17 +134,21 @@ inputManager.addListener(this, "Button LS", "Button RS");
 
 
     public void onAnalog(String name, float value, float tpf) {
-            if(name.equals("Up"))
+         
+            if(name.equals("LS Right"))
             {
-                geom.move(0, tpf/5, 0);
+                vehicle.vehicleNode.rotate(0,-tpf, 0);
             }
-            if(name.equals("Right"))
+            if(name.equals("LS Left"))
             {
-                geom.move(tpf/5, 0, 0);
+                vehicle.vehicleNode.rotate(0,tpf, 0);
             }
-            if(name.equals("Down"))
+            if(name.equals("Trigger R"))
             {
-                geom.move(0, -tpf/5, 0);
+                Quaternion q = vehicle.vehicleNode.getLocalRotation();
+                
+                vehicle.vehicleNode.move(-tpf * 5, 0, 0);
             }
+            
     }
 }
--- src/mygame/PlayerVehicle.java
+++ src/mygame/PlayerVehicle.java
@@ -6,8 +6,10 @@ package mygame;
 
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
+import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 
 /**
  *
@@ -29,12 +31,37 @@ public class PlayerVehicle
         currentState = SELECTION;
         vehicleNode = new Node();
         this.ID = ID;
+        vehicleNode = (Node)m.getAssetManager().loadModel("Models/CARS222.j3o");
+        vehicleNode.setShadowMode(RenderQueue.ShadowMode.Cast);
+        vehicleNode.setLocalTranslation(0, 5f, 0);
+        vehicleNode.rotate(0,0, 0);
+//        Geometry chasis = findGeom(vehicleNode, "BDY");
+       // vehicleGeom = findGeom(vehicleNode, "BDY");
+       // vehicleNode.attachChild(vehicleGeom);
+        m.getRootNode().attachChild(vehicleNode);
     }
+    
     public void onAction(String name, boolean isPressed, float tpf) {
         
     }
 
     public void onAnalog(String name, float value, float tpf) {
     }
-    
+    private Geometry findGeom(Spatial spatial, String name) {
+        if (spatial instanceof Node) {
+            Node node = (Node) spatial;
+            for (int i = 0; i < node.getQuantity(); i++) {
+                Spatial child = node.getChild(i);
+                Geometry result = findGeom(child, name);
+                if (result != null) {
+                    return result;
+                }
+            }
+        } else if (spatial instanceof Geometry) {
+            if (spatial.getName().startsWith(name)) {
+                return (Geometry) spatial;
+            }
+        }
+        return null;
+    }
 }
